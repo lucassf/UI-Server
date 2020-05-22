cc11b37c3763eebb80f27b0799843e6217295161
Lucas Soares <lucassoares-2010@hotmail.com>
2020-05-21 22:47:55 -0300

    Express sessions

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var dboper = require('./operations');
+var session = require('express-session')
+var FileStore = require('session-file-store')(session)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +19,7 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected to server')
-}, (err) => {console.log(err);})
+}, (err) => { console.log(err); })
 var app = express();
 
 // view engine setup
@@ -28,7 +29,51 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('12345-67890-09876-54322'));
+
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54322',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}))
+
+function auth(req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        console.log('here')
+        var authHeader = req.headers.authorization;
+        var error = true;
+
+        if (authHeader) {
+            var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+
+            if (auth[0] === 'admin' && auth[1] == 'password') {
+                error = false;
+                req.session.user = 'admin';
+                next();
+            }
+        }
+        if (error) {
+            var err = new Error('You are not authenticated');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            return next(err);
+        }
+    } else {
+        if (req.session.user == 'admin'){
+            next();
+        } else {
+            var err = new Error('You are not authenticated');
+            err.status = 401;
+            return next(err);
+        }
+    }
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
